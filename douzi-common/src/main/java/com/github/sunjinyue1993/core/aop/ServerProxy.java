1373a1f0a5e5c5e05cc65e766b2518d1038d81d2
SFS_Bean <sunjinyue1993@outlook.com>
2020-09-15 15:44:35 +0800

    删除core模块

--- douzi-common/src/main/java/com/github/sunjinyue1993/core/aop/ServerProxy.java
+++ douzi-common/src/main/java/com/github/sunjinyue1993/core/aop/ServerProxy.java
@@ -1,9 +1,23 @@
 package com.github.sunjinyue1993.core.aop;
 
+import org.apache.zookeeper.ZooKeeper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
 /**
  * @description:
  * @author: admin
  * @date: Created in 2020/9/14 下午 11:31
  */
+@Component
 public class ServerProxy {
+
+    @Autowired
+    private ApplicationContext zkConnection;
+
+    public ZooKeeper getZkConnect(){
+        return (ZooKeeper) zkConnection.getBean("zkConnection");
+    }
+
 }
